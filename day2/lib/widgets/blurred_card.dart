910c3afab16cb4af94a52a8cdda97301702cf87c
ahmad-cod <ahmadadewale800@gmail.com>
2024-06-07 16:09:00 +0100

    feat: Create reusable widget for wallet cards

---
+++ day2/lib/widgets/blurred_card.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class BlurredCard extends StatelessWidget {
+  const BlurredCard({super.key, required this.position});
+
+  final String position;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(children: [
+          SvgPicture.asset(
+            'lib/images/card$position.svg',
+            width: 300,
+            height: 200,
+          ),
+          Positioned(
+            bottom: 7,
+            left: 7,
+            child: Image.asset('lib/images/blur$position.png', height: 90,)
+          )
+        ]);
+  }
+}
